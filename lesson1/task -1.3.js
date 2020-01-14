f0bc59e1e8fefbbd655754333a009f656d39b7ae
AnastasiaIskovych <59798158+AnastasiaIskovych@users.noreply.github.com>
2020-01-14 23:16:16 +0200

    files for homework 1

---
+++ lesson1/task -1.3.js
@@ -0,0 +1,13 @@
+/*task #3 - declaring variables, displaying them, assigning the value of one variable to another
+ variable and displaying them again*/
+let a = 345;
+let b = "Summer"
+alert(a);
+alert(b);
+a = b;
+alert(a);
+alert(b);
+
+
+/*task #4 - asking about the age with (confirm) function */
+var isAdult = confirm("Are you 18+?")
